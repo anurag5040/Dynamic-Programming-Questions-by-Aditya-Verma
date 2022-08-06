68baf37e84af01168522bfc330b17aeeb0a95ac5
Anurag Kumar <76868552+anurag5040@users.noreply.github.com>
2022-08-06 11:05:55 +0530

    added recursive code

--- LCSubstring.cpp
+++ LCSubstring.cpp
@@ -31,3 +31,32 @@ signed main() {
 	cout << LCSubstr(X, Y, n, m) << endl;
 	return 0;
 }
+
+
+/*
+
+ int solve(string &s, string &t, int n, int m, int &ans, vector<vector<int>> &dp) {
+        if(n==0 || m==0)    return 0;
+        if(dp[n][m]!=-1)    return dp[n][m];
+        
+        int temp=0;
+        if(s[n-1]==t[m-1]) {
+            temp= 1+solve(s, t, n-1, m-1, ans, dp);
+        }
+        
+        solve(s, t, n-1, m, ans, dp);
+        solve(s, t, n, m-1, ans, dp);
+        
+        ans=max(ans, temp);
+        return dp[n][m]=temp;
+    }
+    
+    int longestCommonSubstr(string s, string t, int n, int m)
+    {
+        int ans=0;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        solve(s, t, n, m, ans, dp);
+        return ans;
+    }
+    
+    */
